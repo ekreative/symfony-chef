2994535a2dd394d431b46210b9e45eb03e3622d5
Fred Cox <fred@ekreative.com>
2014-08-13 12:30:05 +0300

    include the database params

--- symfony/recipes/parameters.rb
+++ symfony/recipes/parameters.rb
@@ -1,6 +1,6 @@
 node[:deploy].each do |app_name, deploy|
     template "#{deploy[:deploy_to]}/current/config/parameters.yml" do
-        source "parameters.yml"
+        source "parameters.yml.erb"
         mode 0644
         mode 0660
         group deploy[:group]
@@ -12,7 +12,14 @@ node[:deploy].each do |app_name, deploy|
         end
 
         variables(
-            :params => node[:vblogger].to_json
+            :params => node[:vblogger].merge({
+                :database_driver => "pdo_mysql"
+                :database_host => deploy[:database][:host]
+                :database_port: deploy[:database][:port]
+                :database_name: deploy[:database][:dbname]
+                :database_user: deploy[:database][:username]
+                :database_password: deploy[:database][:password]
+            }).to_json
         )
 
         only_if do
