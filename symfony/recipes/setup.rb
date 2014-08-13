6c1880af42b44f2266be377ed71c77763dfde6e6
Fred Cox <fred@ekreative.com>
2014-08-13 10:52:29 +0300

    first try

---
+++ symfony/recipes/setup.rb
@@ -0,0 +1,8 @@
+node[:deploy].each do |app_name, deploy|
+    composer_project "#{deploy[:deploy_to]}/current" do
+        dev false
+        quiet true
+        optimize_autoloader true
+        action :install
+    end
+end
