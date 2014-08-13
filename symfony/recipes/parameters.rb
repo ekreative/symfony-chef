8ac578bebb705eb5bd498359372a8ee1ddd731fa
Fred Cox <fred@ekreative.com>
2014-08-13 12:53:59 +0300

    params in correct place

--- symfony/recipes/parameters.rb
+++ symfony/recipes/parameters.rb
@@ -1,5 +1,5 @@
 node[:deploy].each do |app_name, deploy|
-    template "#{deploy[:deploy_to]}/current/config/parameters.yml" do
+    template "#{deploy[:deploy_to]}/current/app/config/parameters.yml" do
         source "parameters.yml.erb"
         mode 0644
         mode 0660
@@ -12,18 +12,20 @@ node[:deploy].each do |app_name, deploy|
         end
 
         variables(
-            :params => node[:vblogger].merge({
-                :database_driver => "pdo_mysql",
-                :database_host => deploy[:database][:host],
-                :database_port => deploy[:database][:port],
-                :database_name => deploy[:database][:dbname],
-                :database_user => deploy[:database][:username],
-                :database_password => deploy[:database][:password]
-            }).to_json
+            :params => {
+                :parameters => node[:vblogger][:parameters].merge({
+                    :database_driver => "pdo_mysql",
+                    :database_host => deploy[:database][:host],
+                    :database_port => deploy[:database][:port],
+                    :database_name => deploy[:database][:dbname],
+                    :database_user => deploy[:database][:username],
+                    :database_password => deploy[:database][:password]
+                })
+            }.to_json
         )
 
         only_if do
-             File.directory?("#{deploy[:deploy_to]}/current/config")
+             File.directory?("#{deploy[:deploy_to]}/current/app/config")
         end
     end
 end
