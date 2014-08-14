a1b0f662a641c153460baab85a51f17de0a0d6f1
Fred Cox <fred@ekreative.com>
2014-08-14 09:26:49 +0300

    add some log

--- symfony/recipes/composer.rb
+++ symfony/recipes/composer.rb
@@ -1,4 +1,6 @@
 node[:deploy].each do |app_name, deploy|
+    log "going to compose"
+    log "going to use #{node['composer']['bin']}"
     composer_project "#{deploy[:deploy_to]}/current" do
         group deploy[:group]
         if platform?("ubuntu")
@@ -10,4 +12,6 @@ node[:deploy].each do |app_name, deploy|
         quiet false
         action :install
     end
+
+    log "composed"
 end
