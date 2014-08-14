e70dfbdb4f4dd1c61186719e12fe4498227f7a0b
Fred Cox <fred@ekreative.com>
2014-08-14 12:58:11 +0300

    composer as default user

--- symfony/recipes/composer.rb
+++ symfony/recipes/composer.rb
@@ -1,11 +1,5 @@
 node[:deploy].each do |app_name, deploy|
     composer_project "#{deploy[:deploy_to]}/current" do
-        group deploy[:group]
-        if platform?("ubuntu")
-            user "www-data"
-        elsif platform?("amazon")
-            user "apache"
-        end
         dev true
         quiet false
         action :install
