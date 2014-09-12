6f21d797aff4679e00ef2330e6d6b69444aeb104
Fred Cox <fred@ekreative.com>
2014-09-12 12:48:04 +0300

    assetic perms

--- symfony/recipes/assetic.rb
+++ symfony/recipes/assetic.rb
@@ -3,12 +3,12 @@ node[:deploy].each do |app_name, deploy|
       action :delete
   end
   execute "assetic" do
-      group deploy[:group]
-      if platform?("ubuntu")
-          user "www-data"
-      elsif platform?("amazon")
-          user "apache"
-      end
+      # group deploy[:group]
+      # if platform?("ubuntu")
+      #     user "www-data"
+      # elsif platform?("amazon")
+      #     user "apache"
+      # end
 
       cwd "#{deploy[:deploy_to]}/current"
       command "#{deploy[:deploy_to]}/current/bin/console assetic:dump --env=prod"
