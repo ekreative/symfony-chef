0797d7174d40b279553fdd3728247d6a1e026d69
Fred Cox <fred@ekreative.com>
2014-09-12 11:10:51 +0300

    link the right way around

--- symfony/recipes/legacylinks.rb
+++ symfony/recipes/legacylinks.rb
@@ -1,29 +1,29 @@
 node[:deploy].each do |app_name, deploy|
-    link "#{deploy[:deploy_to]}/current/var/cache" do
+    link "#{deploy[:deploy_to]}/current/app/cache" do
         group deploy[:group]
         if platform?("ubuntu")
             owner "www-data"
         elsif platform?("amazon")
             owner "apache"
         end
-        to "#{deploy[:deploy_to]}/current/app/cache"
+        to "#{deploy[:deploy_to]}/current/var/cache"
     end
-    link "#{deploy[:deploy_to]}/current/bin/console" do
+    link "#{deploy[:deploy_to]}/current/app/console" do
         group deploy[:group]
         if platform?("ubuntu")
             owner "www-data"
         elsif platform?("amazon")
             owner "apache"
         end
-        to "#{deploy[:deploy_to]}/current/app/console"
+        to "#{deploy[:deploy_to]}/current/bin/console"
     end
-    link "#{deploy[:deploy_to]}/current/var/logs" do
+    link "#{deploy[:deploy_to]}/current/app/logs" do
         group deploy[:group]
         if platform?("ubuntu")
             owner "www-data"
         elsif platform?("amazon")
             owner "apache"
         end
-        to "#{deploy[:deploy_to]}/current/app/logs"
+        to "#{deploy[:deploy_to]}/current/var/logs"
     end
 end
