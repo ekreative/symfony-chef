f696d95502636ab9f6561ab99015d76f2821d583
Fred Cox <fred@ekreative.com>
2014-09-30 15:55:17 +0300

    Add config for php cli

--- php/recipes/ini.rb
+++ php/recipes/ini.rb
@@ -3,3 +3,8 @@ template "/etc/php5/apache2/conf.d/50-custom.ini" do
   mode 0644
   mode 0660
 end
+template "/etc/php5/cli/conf.d/50-custom.ini" do
+  source "custom.ini.erb"
+  mode 0644
+  mode 0660
+end
