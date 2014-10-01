d39c5753710c6ab1d4a5d0617ccfebf915c7da4f
Fred Cox <fred@ekreative.com>
2014-10-01 10:28:24 +0300

    Create ini files more carefully

--- php/recipes/ini.rb
+++ php/recipes/ini.rb
@@ -1,10 +1,9 @@
-template "/etc/php5/apache2/conf.d/50-custom.ini" do
-  source "custom.ini.erb"
-  mode 0644
-  mode 0660
-end
-template "/etc/php5/cli/conf.d/50-custom.ini" do
-  source "custom.ini.erb"
-  mode 0644
-  mode 0660
+["/etc/php5/apache2", "/etc/php5/cli"].each do |conf_dir|
+  if Dir.exist? conf_dir
+    template "#{conf_dir}/conf.d/50-custom.ini" do
+      source "custom.ini.erb"
+      mode 0644
+      mode 0660
+    end
+  end
 end
