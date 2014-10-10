8b3e8e109704ef3d3769e48d58eb7c5a965f3a84
Yuriy Tarnavskiy <yuriy.tarnavskiy@ekreative.com>
2014-10-10 16:01:17 +0300

    add new parameters file for backup instance

---
+++ symfony/recipes/parameters2.rb
@@ -0,0 +1,26 @@
+node[:deploy].each do |app_name, deploy|
+    template "#{deploy[:deploy_to]}/current/app/config/parameters.yml" do
+        source "parameters2.yml.erb"
+        mode 0644
+        mode 0660
+        group deploy[:group]
+
+        if platform?("ubuntu")
+          owner "www-data"
+        elsif platform?("amazon")
+          owner "apache"
+        end
+
+        variables(
+            :params => {
+                :parameters => {
+                    :nodejs => "/usr/bin/node"
+                }.merge(node[app_name][:parameters])
+            }.to_json
+        )
+
+        only_if do
+             File.directory?("#{deploy[:deploy_to]}/current/app/config")
+        end
+    end
+end
