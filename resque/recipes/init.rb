6252dccca163bd85321248985a1e3c7f7c50f45c
Yuriy Tarnavskiy <yuriy.tarnavskiy@ekreative.com>
2014-10-14 17:20:00 +0300

    added init script (stop, configure, start)

---
+++ resque/recipes/init.rb
@@ -0,0 +1,40 @@
+service "supervisor" do
+  action :stop
+end
+
+node[:deploy].each do |app_name, deploy|
+    template "#{deploy[:deploy_to]}/current/app/config/parameters.yml" do
+        source "parameters.yml.erb"
+        mode 0644
+        mode 0660
+        group deploy[:group]
+
+        if platform?("ubuntu")
+          owner "www-data"
+        elsif platform?("amazon")
+          owner "apache"
+        end
+
+        variables(
+            :params => {
+                :parameters => {
+                    :database_driver => "pdo_mysql",
+                    :database_host => deploy[:database][:host],
+                    :database_port => deploy[:database][:port],
+                    :database_name => deploy[:database][:database],
+                    :database_user => deploy[:database][:username],
+                    :database_password => deploy[:database][:password],
+                    :nodejs => "/usr/bin/node"
+                }.merge(node[app_name][:parameters])
+            }.to_json
+        )
+
+        only_if do
+             File.directory?("#{deploy[:deploy_to]}/current/app/config")
+        end
+    end
+end
+
+service "supervisor" do
+  action :start
+end
