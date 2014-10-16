ef6f395af387263d2ad2c58e5b7fe68558fd3dc8
Yuriy Tarnavskiy <yuriy.tarnavskiy@ekreative.com>
2014-10-16 10:19:20 +0300

    fix init script

--- resque/recipes/init.rb
+++ resque/recipes/init.rb
@@ -3,35 +3,46 @@ service "supervisor" do
 end
 
 node[:deploy].each do |app_name, deploy|
-    template "#{deploy[:deploy_to]}/current/app/config/parameters.yml" do
-        source "parameters.yml.erb"
+    template "/etc/supervisor/conf.d/#{app_name}.conf" do
+        source "process.conf.erb"
         mode 0644
         mode 0660
         group deploy[:group]
 
         if platform?("ubuntu")
-          owner "www-data"
+          user = "www-data"
         elsif platform?("amazon")
-          owner "apache"
+          user = "apache"
         end
 
         variables(
-            :params => {
-                :parameters => {
-                    :database_driver => "pdo_mysql",
-                    :database_host => deploy[:database][:host],
-                    :database_port => deploy[:database][:port],
-                    :database_name => deploy[:database][:database],
-                    :database_user => deploy[:database][:username],
-                    :database_password => deploy[:database][:password],
-                    :nodejs => "/usr/bin/node"
-                }.merge(node[app_name][:parameters])
-            }.to_json
+            :name => app_name,
+            :command => "#{deploy[:deploy_to]}/current/bin/resque",
+            :number => 2,
+            :user => user,
+            :queue => node[app_name][:parameters][:resque_default_queue],
+            :backend => "#{node[app_name][:parameters][:redis_host]}:#{node[app_name][:parameters][:redis_port]}"
         )
+    end
+    template "/etc/supervisor/conf.d/#{app_name}-scheduler.conf" do
+        source "process.conf.erb"
+        mode 0644
+        mode 0660
+        group deploy[:group]
 
-        only_if do
-             File.directory?("#{deploy[:deploy_to]}/current/app/config")
+        if platform?("ubuntu")
+          user = "www-data"
+        elsif platform?("amazon")
+          user = "apache"
         end
+
+        variables(
+            :name => "#{app_name}-scheduler",
+            :command => "#{deploy[:deploy_to]}/current/bin/resque-scheduler",
+            :number => 1,
+            :user => user,
+            :backend => "#{node[app_name][:parameters][:redis_host]}:#{node[app_name][:parameters][:redis_port]}"
+        )
     end
 end
 
