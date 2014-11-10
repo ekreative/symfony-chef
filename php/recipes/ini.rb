cd789483888b3bd34bbb2859d903a383e18c027a
Yuriy Tarnavskiy <yuriy.tarnavskiy@ekreative.com>
2014-11-10 12:51:45 +0200

    php::ini ready

--- php/recipes/ini.rb
+++ php/recipes/ini.rb
@@ -7,7 +7,6 @@
 end
 
 # ["/etc/php5/apache2", "/etc/php5/cli"].each do |conf_dir|
-#   Dir.mkdir(conf_dir)
 #   if Dir.exist? conf_dir
 #     template "#{conf_dir}/conf.d/50-custom.ini" do
 #       source "custom.ini.erb"
