3ba0eb9a9404f1e50d6946f4fe7b16ff6778ff3a
Yuriy Tarnavskiy <yuriy.tarnavskiy@ekreative.com>
2014-11-10 09:25:15 +0200

    php.ini experement

--- php/recipes/ini.rb
+++ php/recipes/ini.rb
@@ -1,4 +1,6 @@
 ["/etc/php5/apache2", "/etc/php5/cli"].each do |conf_dir|
+  Dir.mkdir(conf_dir)
+  puts "zzzttt"
   if Dir.exist? conf_dir
     template "#{conf_dir}/conf.d/50-custom.ini" do
       source "custom.ini.erb"
