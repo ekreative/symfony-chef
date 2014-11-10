e212cf23a09b5d77d73526e6806e3c104d2b89c0
Yuriy Tarnavskiy <yuriy.tarnavskiy@ekreative.com>
2014-11-10 12:01:28 +0200

    php.ini experement6

--- php/recipes/ini.rb
+++ php/recipes/ini.rb
@@ -1,4 +1,15 @@
-node["/etc/php5/apache2", "/etc/php5/cli"].each do |conf_dir|
+node["/etc/php5/apache2"].each do |conf_dir|
+  Dir.mkdir(conf_dir)
+  if Dir.exist? conf_dir
+    template "#{conf_dir}/conf.d/50-custom.ini" do
+      source "custom.ini.erb"
+      mode 0644
+      mode 0660
+    end
+  end
+end
+
+node["/etc/php5/cli"].each do |conf_dir|
   Dir.mkdir(conf_dir)
   if Dir.exist? conf_dir
     template "#{conf_dir}/conf.d/50-custom.ini" do
