7719dbe0449f2fedd1832991ed9b7fdf91d84ce1
Yuriy Tarnavskiy <yuriy.tarnavskiy@ekreative.com>
2014-11-10 12:33:02 +0200

    php.ini experement8

--- php/recipes/ini.rb
+++ php/recipes/ini.rb
@@ -1,12 +1,9 @@
-["/etc/php5/apache2"].each do |conf_dir|
-  # Dir.mkdir(conf_dir)
-  # if Dir.exist? conf_dir
+["/etc/php5/apache2", "/etc/php5/cli"].each do |conf_dir|
     template "#{conf_dir}/conf.d/50-custom.ini" do
       source "custom.ini.erb"
       mode 0644
       mode 0660
     end
-  # end
 end
 
 # ["/etc/php5/apache2", "/etc/php5/cli"].each do |conf_dir|
