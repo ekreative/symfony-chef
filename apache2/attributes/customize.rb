8aa2c73da3432a4b9ae6498b8b4ccc57ea6716ca
Fred Cox <fred@ekreative.com>
2015-02-25 10:42:10 +0200

    use 20

--- apache2/attributes/customize.rb
+++ apache2/attributes/customize.rb
@@ -1,2 +1,2 @@
-normal[:apache][:prefork][:maxrequestworkers] = 19
-normal[:apache][:prefork][:serverlimit] = 19
+normal[:apache][:prefork][:maxrequestworkers] = 20
+normal[:apache][:prefork][:serverlimit] = 20
