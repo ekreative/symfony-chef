13100120096222def77cf4f32530ae39123239b4
Fred Cox <mcfedr@gmail.com>
2015-02-25 12:38:59 +0200

    need config before install

--- logs/recipes/restart.rb
+++ logs/recipes/restart.rb
@@ -1,6 +1,4 @@
-service 'awslogs' do
-  action :stop
-end
-service 'awslogs' do
-  action :start
+execute "Install CloudWatch Logs agent" do
+  command "/opt/aws/cloudwatch/awslogs-agent-setup.py -n -r eu-west-1 -c /tmp/cwlogs.cfg"
+  not_if { system "pgrep -f aws-logs-agent-setup" }
 end
