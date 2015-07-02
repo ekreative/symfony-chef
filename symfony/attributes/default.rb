2ded324f2eb22b56950818d4e6a769619c35fc8a
Fred Cox <mcfedr@gmail.com>
2015-07-03 00:49:30 +0300

    rename attrs

--- symfony/attributes/default.rb
+++ symfony/attributes/default.rb
@@ -1,3 +1,3 @@
-default[:php][:packages] = ['php5-intl', 'php5-redis', 'php5-apcu', 'php5-cli']
-default[:php][:ini][:memorylimit] = '128MB'
-default[:php][:ini][:timezone] = 'UTC'
+default[:symfony][:packages] = ['php5-intl', 'php5-redis', 'php5-apcu', 'php5-cli']
+default[:symfony][:ini][:memorylimit] = '128MB'
+default[:symfony][:ini][:timezone] = 'UTC'
