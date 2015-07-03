837dc040246e1c6c0b65c97be9a2ae9a7d17892f
Fred Cox <mcfedr@gmail.com>
2015-07-03 09:30:41 +0300

    writable dirs are configurable

--- symfony/recipes/permissions.rb
+++ symfony/recipes/permissions.rb
@@ -1,13 +1,15 @@
 node[:deploy].each do |app_name, deploy|
-  directory "#{deploy[:deploy_to]}/current/app/cache" do
-      group deploy[:group]
-      if platform?("ubuntu")
-          user "www-data"
-      elsif platform?("amazon")
-          user "apache"
-      end
-      recursive true
-  end
-  execute "chmod -R g+w #{deploy[:deploy_to]}/current/app/cache"
   execute "chmod -R g+w #{deploy[:deploy_to]}/shared/log"
+  node[:symfony][:writable].each do |dir|
+      directory "#{deploy[:deploy_to]}/current/#{dir}" do
+          group deploy[:group]
+          if platform?("ubuntu")
+              user "www-data"
+          elsif platform?("amazon")
+              user "apache"
+          end
+          recursive true
+      end
+      execute "chmod -R g+w #{deploy[:deploy_to]}/current/#{dir}"
+  end
 end
