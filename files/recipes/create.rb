63b874ea606308cde433c6c7e2556d6d07460132
Fred Cox <mcfedr@gmail.com>
2015-07-03 17:04:37 +0300

    Add missing check for existence

--- files/recipes/create.rb
+++ files/recipes/create.rb
@@ -1,5 +1,5 @@
 node[:deploy].each do |app_name, deploy|
-    if node[app_name][:files].present?
+    if node[app_name].present? and node[app_name][:files].present?
         node[app_name][:files].each do |name, content|
             template "#{deploy[:deploy_to]}/current/#{name}" do
                 source "content.erb"
