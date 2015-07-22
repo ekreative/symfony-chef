0a999d8ea04f05cbab7cf300464bf65b2e3574dd
Fred Cox <mcfedr@gmail.com>
2015-07-22 15:05:17 +0300

    change to source install

--- symfony/recipes/composer.rb
+++ symfony/recipes/composer.rb
@@ -2,7 +2,7 @@ node[:deploy].each do |app_name, deploy|
     composer_project "#{deploy[:deploy_to]}/current" do
         dev true
         quiet false
-        prefer_dist true
+        prefer_source true
         action :install
         only_if do File.exists?("#{deploy[:deploy_to]}/current/composer.json") end
     end
