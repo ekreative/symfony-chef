517a0afab5f6ac964e102d4ccea1baaacd921a95
Fred Cox <fred@ekreative.com>
2015-07-28 14:28:03 +0300

    Support non-symfony cron jobs

--- symfony/recipes/cron.rb
+++ symfony/recipes/cron.rb
@@ -7,13 +7,19 @@ node[:deploy].each do |app_name, deploy|
 
     if node[app_name].present? and node[app_name][:crons].present?
         node[app_name][:crons].each do |cron|
+            if cron[:symfony].present? and !cron[:symfony]
+                cmd = "#{cron[:command]} >> #{deploy[:deploy_to]}/shared/log/cron.log"
+            else
+                cmd = "#{deploy[:deploy_to]}/current/#{node[:symfony][:console]} #{cron[:command]} --env=prod -vv >> #{deploy[:deploy_to]}/shared/log/cron.log"
+            end
+
             cron "symfony-#{cron[:name] || cron[:command]}" do
                 minute cron[:minute] || '*'
                 hour cron[:hour] || '*'
                 day cron[:day] || '*'
                 month cron[:month] || '*'
                 weekday cron[:weekday] || '*'
-                command "#{deploy[:deploy_to]}/current/#{node[:symfony][:console]} #{cron[:command]} --env=prod -vv >> #{deploy[:deploy_to]}/shared/log/cron.log"
+                command cmd
                 user user
             end
         end
