8b24b337922f46a7e1462f28eca0c98e80eaa76a
Fred Cox <fred@ekreative.com>
2015-08-11 15:17:34 +0300

    correct user for assetic

--- symfony/recipes/assetic.rb
+++ symfony/recipes/assetic.rb
@@ -3,8 +3,12 @@ node[:deploy].each do |app_name, deploy|
         cwd "#{deploy[:deploy_to]}/current"
         command "#{deploy[:deploy_to]}/current/#{node[:symfony][:console]} assetic:dump --env=prod"
         returns [0, 1]
-        user deploy[:user]
         group deploy[:group]
+        if platform?("ubuntu")
+            user "www-data"
+        elsif platform?("amazon")
+            user "apache"
+        end
         only_if do File.exists?("#{deploy[:deploy_to]}/current/#{node[:symfony][:console]}") end
     end
 end
