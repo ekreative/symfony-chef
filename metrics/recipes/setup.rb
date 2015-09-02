e1be002446902be5813f8d7feda9d7035f7cd7e5
Fred Cox <fred@ekreative.com>
2015-09-02 12:29:52 +0300

    Add scripts to use extra cloudwatch metrics

---
+++ metrics/recipes/setup.rb
@@ -0,0 +1,24 @@
+['unzip', 'libwww-perl', 'libdatetime-perl'].each do |package_name|
+    package package_name do
+        action :install
+    end
+end
+
+
+directory "/opt/aws/cloudwatch-metrics" do
+    recursive true
+end
+
+remote_file "/opt/aws/cloudwatch-metrics/CloudWatchMonitoringScripts.zip" do
+    source node[:metrics][:script_url]
+end
+
+execute "Unzip the scripts" do
+    command "unzip /opt/aws/cloudwatch-metrics/CloudWatchMonitoringScripts.zip"
+    cwd "/opt/aws/cloudwatch-metrics"
+end
+
+cron "cloudwatch-metrics" do
+    minute '*/5'
+    command '/opt/aws/cloudwatch-metrics/mon-put-instance-data.pl --disk-path=/ --disk-space-util --disk-space-used --disk-space-avail --from-cron'
+end
