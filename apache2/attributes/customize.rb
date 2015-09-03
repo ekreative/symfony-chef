3da0814e6be87bf2b6a0034ba3625b2acd01721c
Fred Cox <fred@ekreative.com>
2015-09-03 12:51:32 +0300

    Skip health checks in the apache logs
    Also increase error log level to notice

--- apache2/attributes/customize.rb
+++ apache2/attributes/customize.rb
@@ -1,2 +1,4 @@
 normal[:apache][:prefork][:maxrequestworkers] = `free | awk '/^Mem:/{print $2}'`.to_i / 81920
 normal[:apache][:prefork][:serverlimit] = normal[:apache][:prefork][:maxrequestworkers]
+normal[:apache][:log_level] = 'notice'
+normal[:apache][:log_filter] = [{'User-Agent' => 'ELB-HealthChecker'}, {'User-Agent' => 'Amazon Route 53 Health Check Service'}]
