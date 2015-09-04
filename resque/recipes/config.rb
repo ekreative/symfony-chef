742036c3bcd4270ffe9b5d369e6d1172ce2e99d5
Fred Cox <fred@ekreative.com>
2015-09-04 16:10:50 +0300

    Fix object syntax

--- resque/recipes/config.rb
+++ resque/recipes/config.rb
@@ -26,7 +26,7 @@ node[:deploy].each do |app_name, deploy|
                 :queue => node[app_name][:resque][:queue] || node[:resque][:queue],
                 :backend => "#{host}:#{port}",
                 :app_include => app_include && "#{deploy[:deploy_to]}/current/#{app_include}",
-                :prefix = node[app_name][:resque][:prefix]
+                :prefix => node[app_name][:resque][:prefix]
             )
         end
         if node[app_name][:resque][:scheduler]
@@ -39,7 +39,7 @@ node[:deploy].each do |app_name, deploy|
                     :number => 1,
                     :user => user,
                     :backend => "#{host}:#{port}",
-                    :prefix = node[app_name][:resque][:prefix]
+                    :prefix => node[app_name][:resque][:prefix]
                 )
             end
         end
