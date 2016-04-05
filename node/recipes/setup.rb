130851cdfb31eb5bacca6fc80a61ff746b5b4340
Fred Cox <fred@ekreative.com>
2016-04-05 15:30:49 +0300

    support amazon platform for node5

--- node/recipes/setup.rb
+++ node/recipes/setup.rb
@@ -1,5 +1,11 @@
+if platform?("ubuntu")
+    source = node[:node][:source][:ubuntu]
+elsif platform?("amazon")
+    source = node[:node][:source][:amazon]
+end
+
 execute "Setup node packages" do
-    command "curl -sL #{node[:node][:source]} | bash -"
+    command "curl -sL #{source} | bash -"
 end
 
 node[:node][:packages].each do |package_name|
