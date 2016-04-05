da7c58efc7cd67ecf907afb9a27e266f9534f7df
Fred Cox <fred@ekreative.com>
2016-04-05 15:06:10 +0300

    Use nodejs 5

--- node/attributes/default.rb
+++ node/attributes/default.rb
@@ -1,3 +1,4 @@
 default[:node][:bower_bin] = 'node_modules/.bin/bower'
 default[:node][:gulp_bin] = 'node_modules/.bin/gulp'
-default[:node][:packages] = ['nodejs', 'npm', 'nodejs-legacy']
+default[:node][:packages] = ['nodejs']
+default[:node][:source] = 'https://deb.nodesource.com/setup_5.x'
--- node/recipes/setup.rb
+++ node/recipes/setup.rb
@@ -1,3 +1,7 @@
+execute "Setup node packages" do
+    command "curl -sL #{node[:node][:source]} | bash -"
+end
+
 node[:node][:packages].each do |package_name|
     package package_name do
         action :install
