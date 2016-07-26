3c3363f5486303370abd138cb56283b202276d12
Pavel Hudinsky <p.hudinsky@gmail.com>
2016-07-26 18:05:10 +0300

    Fix syntax of memory limit value

--- symfony/attributes/default.rb
+++ symfony/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:symfony][:packages] = ['php5-intl', 'php5-redis', 'php5-apcu', 'php5-cli']
-default[:symfony][:ini][:memorylimit] = '128MB'
+default[:symfony][:ini][:memorylimit] = '128M'
 default[:symfony][:ini][:timezone] = 'UTC'
 default[:symfony][:console] = 'app/console'
 default[:symfony][:writable] = ['app/cache']
