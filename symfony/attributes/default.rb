16bf835adce0ed3fdb33b9ce75672a6a4e1fe068
Pavel Hudinsky <p.hudinsky@gmail.com>
2016-07-27 14:22:57 +0300

    Fix names of php ini settings (#4)

--- symfony/attributes/default.rb
+++ symfony/attributes/default.rb
@@ -1,6 +1,6 @@
 default[:symfony][:packages] = ['php5-intl', 'php5-redis', 'php5-apcu', 'php5-cli']
-default[:symfony][:ini][:memorylimit] = '128M'
-default[:symfony][:ini][:timezone] = 'UTC'
+default[:symfony][:ini]['memory_limit'] = '128M'
+default[:symfony][:ini]['date.timezone'] = 'UTC'
 default[:symfony][:console] = 'app/console'
 default[:symfony][:writable] = ['app/cache']
 default[:symfony][:composer_dev] = true
