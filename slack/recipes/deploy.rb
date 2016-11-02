841d0fbe09e87ac23bdf864dc2c0f9c2816e66e8
Fred Cox <fred@ekreative.com>
2016-11-02 15:55:31 +0200

    Add stack name to notification

--- slack/recipes/deploy.rb
+++ slack/recipes/deploy.rb
@@ -1,7 +1,7 @@
 node[:deploy].each do |app_name, deploy|
     unless node[:slack][:hook].nil?
         message = {
-            :text => "Deployed #{app_name}"
+            :text => "Deployed #{app_name} on stack #{node["opsworks"]["stack"]["name"]}"
         }
         unless node[:slack][:channel].nil?
             message[:channel] = node[:slack][:channel]
