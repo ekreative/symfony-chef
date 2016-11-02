4d435f2c9f2dc2538d9e73b9c5a3cd8f335027f1
Fred Cox <fred@ekreative.com>
2016-11-02 15:42:56 +0200

    Fix content type

--- slack/recipes/deploy.rb
+++ slack/recipes/deploy.rb
@@ -1,12 +1,21 @@
 node[:deploy].each do |app_name, deploy|
     unless node[:slack][:hook].nil?
+        message = {
+            :text => "Deployed #{app_name}"
+        }
+        unless node[:slack][:channel].nil?
+            message[:channel] = node[:slack][:channel]
+        end
+        
         http_request 'hook' do
             action :post
             url node[:slack][:hook]
-            message ({
-                :text => "Deployed #{app_name}",
-                :channel => node[:slack][:channel]
-            }.to_json)
+            headers ({
+                "content-type" => "application/json"
+            })
+            message (message.to_json)
+            retries 0
+            ignore_failure true
         end
     end
 end
