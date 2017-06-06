09ba8918c3d9899a4933d5e371435834a1dd040c
Fred Cox <frederick.j.cox@gmail.com>
2017-06-06 17:35:53 +0300

    Default node 8

--- node/attributes/default.rb
+++ node/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:node][:bower_bin] = 'node_modules/.bin/bower'
 default[:node][:gulp_bin] = 'node_modules/.bin/gulp'
 default[:node][:packages] = ['nodejs']
-default[:node][:source][:ubuntu] = 'https://deb.nodesource.com/setup_7.x'
-default[:node][:source][:amazon] = 'https://rpm.nodesource.com/setup_7.x'
+default[:node][:source][:ubuntu] = 'https://deb.nodesource.com/setup_8.x'
+default[:node][:source][:amazon] = 'https://rpm.nodesource.com/setup_8.x'
