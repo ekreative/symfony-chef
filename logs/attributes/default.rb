e005fded4372abb353e606dbdfefea6b18bab8b3
Fred Cox <frederick.j.cox@gmail.com>
2017-06-14 16:01:49 +0300

    Use version 1.3.9 of cloud watch installer as its broken in newer release

--- logs/attributes/default.rb
+++ logs/attributes/default.rb
@@ -3,5 +3,5 @@ default[:logs][:symfony] = true
 default[:logs][:apache] = true
 default[:logs][:symfony_datetime] = '[%Y-%m-%d %H:%M:%S]'
 default[:logs][:install_dir] = '/opt/aws/cloudwatch'
-default[:logs][:script_url] = 'https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py'
+default[:logs][:script_url] = 'https://s3.amazonaws.com/aws-cloudwatch/downloads/1.3.9/awslogs-agent-setup.py'
 default[:logs][:script] = 'awslogs-agent-setup.py'
