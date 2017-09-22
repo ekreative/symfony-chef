39e38b298564d4b923499c542d86af769bd9a7b9
Fred Cox <frederick.j.cox@gmail.com>
2017-09-22 09:31:37 +0300

    Fix attribute syntax

--- telegraf/attributes/default.rb
+++ telegraf/attributes/default.rb
@@ -1,7 +1,7 @@
 default[:telegraf][:source][:ubuntu] = 'https://dl.influxdata.com/telegraf/releases/telegraf_1.4.0-1_amd64.deb'
 default[:telegraf][:source][:amazon] = 'https://dl.influxdata.com/telegraf/releases/telegraf-1.4.0-1.x86_64.rpm'
 default[:telegraf][:database] = "telegraf"
-node[:telegraf][:retention_policy] = ""
-node[:telegraf][:write_consistency] = "any"
-node[:telegraf][:timeout] = "5s"
-node[:telegraf][:apache][:url] = "http://localhost/server-status?auto"
+default[:telegraf][:retention_policy] = ""
+default[:telegraf][:write_consistency] = "any"
+default[:telegraf][:timeout] = "5s"
+default[:telegraf][:apache][:url] = "http://localhost/server-status?auto"
